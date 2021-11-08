e5c53f52008a78274844cd951481e9f64205b588
Murat Kasimov <iokasimov.m@gmail.com>
2021-11-08 19:56:27 +0300

    Define a module with SQL queries

--- REPL.hs
+++ REPL.hs
@@ -12,6 +12,8 @@ import "transformers" Control.Monad.Trans.State (StateT, evalStateT, get, modify
 
 import qualified "text" Data.Text.IO as T (putStrLn)
 
+import Control.SQL.Query (today_time_query, all_unfinished_events, timeline_today_events_query)
+
 data Objective = Objective Int Text deriving (Eq, Show)
 
 instance FromRow Objective where
@@ -35,27 +37,6 @@ data Period = Period String String deriving Show
 
 instance FromRow Period where
 	fromRow = Period <$> field <*> field
-
-today_time_query :: Query
-today_time_query = 
-	"SELECT title, strftime('%H:%M', datetime(SUM (end - start), 'unixepoch')) \
-	\FROM events JOIN objectives on events.objective_id = objectives.id \
-	\WHERE start > strftime('%s', date('now')) AND end IS NOT NULL \
-	\GROUP BY objective_id;"
-
-all_unfinished_events :: Query
-all_unfinished_events =
-	"SELECT id, title, event_id, strftime('%H:%M', start, 'unixepoch', 'localtime') \
-	\FROM events join objectives ON objectives.id = events.objective_id \
-	\WHERE end IS NULL;"
-
-timeline_today_events_query :: Query
-timeline_today_events_query =
-	"SELECT title, strftime('%H:%M', start, 'unixepoch', 'localtime'), \
-	\IFNULL(strftime('%H:%M', end, 'unixepoch', 'localtime'), '..:..'), \
-	\strftime('%H:%M', IFNULL(end, strftime('%s', datetime('now'))) - start,'unixepoch') \
-	\FROM events JOIN objectives on events.objective_id = objectives.id \
-	\WHERE start > strftime('%s', date('now')) AND start < strftime('%s', date('now', '+1 day'));"
 
 print_timeline :: [(Text, Text, Text, Text)] -> IO ()
 print_timeline = void . traverse (T.putStrLn . prepare) where
