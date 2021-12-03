2041f09bcdf0c9532ae26c0cf33870eaa14e3980
Murat Kasimov <iokasimov.m@gmail.com>
2021-12-03 15:30:09 +0300

    Define Accessible instance for list zipper

--- Control/Pandora/Main.hs
+++ Control/Pandora/Main.hs
@@ -15,7 +15,7 @@ import "base" Text.Show (show)
 import "base" System.IO (getChar, putStrLn)
 import "sqlite-simple" Database.SQLite.Simple (Connection, Query, open, query_, execute)
 
-import Control.Pandora.Task (Task)
+import Control.Pandora.Task (Task, Status (DONE))
 import Control.Pandora.SQLite (today_tasks, update_task_status)
 import Control.Pandora.TUI (prepare_terminal, refresh_terminal)
 import Control.Pandora.Utils (list_to_list)
@@ -32,7 +32,7 @@ show_task focused (_ :*: status :*: mode :*: title :*: start :*: stop) =
 	show_task_boundaries 1 begin [] = "[BEGIN: " + begin + "] "
 	show_task_boundaries 1 begin complete = "[BEGIN: " + begin + "] [COMPLETE: " + complete + "] "
 
-display :: Zipper List Task -> IO ()
+display :: Tape List Task -> IO ()
 display tasks = void $ do
 	refresh_terminal
 	putStrLn $ "\n   \ESC[1m\ESC[4m" + "Tasks for today" + "\ESC[0m\n"
@@ -46,23 +46,27 @@ show_title True title = "\n + \ESC[1m\ESC[4m" + title + "\ESC[0m"
 refresh :: TUI ()
 refresh = adapt . display =<< current
 
-type TUI = Environment Connection :> State (Zipper List Task) :> IO
+type TUI = Environment Connection :> State (Tape List Task) :> IO
+
+instance Accessible field Task => Accessible field (Tape List Task) where
+	access = access @field . access @Task . sub @Root
 
 keystroke :: Char -> TUI ()
 keystroke 'j' = adapt # navigation @Right
 keystroke 'k' = adapt # navigation @Left
+keystroke 'D' = void $ zoom @(Tape List Task) # access @Status # replace DONE
 
-complete_task :: Connection -> Int -> IO ()
-complete_task connection tid = execute connection update_task_status (1 :: Int, tid)
+complete :: Connection -> Int -> IO ()
+complete connection tid = execute connection update_task_status (1 :: Int, tid)
 
-navigation :: forall direction . (Morphed (Rotate direction) (Zipper List) (Maybe <:.> Zipper List)) => State (Zipper List Task) ()
-navigation = void . modify $ \z -> resolve @(Zipper List Task) identity z # run (rotate @direction z)
+navigation :: forall direction . (Morphed (Rotate direction) (Tape List) (Maybe <:.> Tape List)) => State (Tape List Task) ()
+navigation = void . modify $ \z -> resolve @(Tape List Task) identity z # run (rotate @direction z)
 
 eventloop = (eventloop !.) =<< keystroke =<< (adapt getChar !.) =<< refresh
 
 main = do
 	connection <- open "facts.db"
 	today <- query_ @Task connection today_tasks
-	let Just tasks = run . into @(Zipper List) # list_to_list empty today
+	let Just tasks = run . into @(Tape List) # list_to_list empty today
 	prepare_terminal
 	(eventloop ! connection) ! tasks
