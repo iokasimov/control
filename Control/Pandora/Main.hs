ee919fca5474504b21979f3998768ba5dba8b735
Murat Kasimov <iokasimov.m@gmail.com>
2021-12-03 09:49:58 +0300

    Simplify functionality: focus only on tasks for today

--- Control/Pandora/Main.hs
+++ Control/Pandora/Main.hs
@@ -14,15 +14,11 @@ import "base" Data.String (String)
 import "base" System.IO (getChar, putStrLn)
 import "sqlite-simple" Database.SQLite.Simple (Query, open, query_)
 
-import Control.Pandora.SQLite (today_tasks)
+import Control.Pandora.SQLite (today_tasks, update_task_status)
 import Control.Pandora.TUI (prepare_terminal, refresh_terminal)
 import Control.Pandora.Utils (list_to_list)
 
 type Task = (Int, Int, Int, String, String, String)
-
-type Cursor = Zipper List Task
-
-type Section = String :*: Cursor
 
 show_task :: Boolean -> Task -> String
 show_task focused (_, status, mode, title, start, stop) =
@@ -41,67 +37,34 @@ show_task focused (_, status, mode, title, start, stop) =
 	show_task_boundaries 1 begin [] = "[BEGIN: " + begin + "] "
 	show_task_boundaries 1 begin complete = "[BEGIN: " + begin + "] [COMPLETE: " + complete + "] "
 
-display :: Zipper List Section -> IO ()
-display sections = void $ do
+display :: Zipper List Task -> IO ()
+display tasks = void $ do
 	refresh_terminal
-	display_section False -<<-<<- (Reverse <-|- view (sub @Left) sections)
-	display_section True -<<-<<- view (sub @Root) sections
-	display_section False -<<-<<- view (sub @Right) sections
-
-display_section :: Boolean -> Section -> IO ()
-display_section active (title :*: tasks) = void $ do
-	putStrLn # show_title active title
+	putStrLn $ "\n   \ESC[1m\ESC[4m" + "Tasks for today" + "\ESC[0m\n"
 	putStrLn . show_task False -<<-<<- (Reverse <-|- view (sub @Left) tasks)
-	putStrLn . show_task active -<<-<<- view (sub @Root) tasks
+	putStrLn . show_task True -<<-<<- view (sub @Root) tasks
 	putStrLn . show_task False -<<-<<- view (sub @Right) tasks
 
 show_title False title = "\n   \ESC[4m" + title + "\ESC[0m"
 show_title True title = "\n + \ESC[1m\ESC[4m" + title + "\ESC[0m"
 
-refresh' :: State (Zipper List Section) :> IO := ()
-refresh' = adapt . display =<< current
+refresh :: State (Zipper List Task) :> IO := ()
+refresh = adapt . display =<< current
 
-type TUI = State Section :> IO
+type TUI = State (Zipper List Task) :> IO
 
---keystroke :: Char -> TUI ()
---keystroke 'j' = adapt # navigation @Right
---keystroke 'k' = adapt # navigation @Left
+keystroke :: Char -> TUI ()
+keystroke 'j' = adapt # navigation @Right
+keystroke 'k' = adapt # navigation @Left
 
-navigation :: Char -> State (Zipper List Section) ()
-navigation 'j' = zoom @(Zipper List Section) (access @Cursor . access @Section . sub @Root) (inner @Right)
-navigation 'k' = zoom @(Zipper List Section) (access @Cursor . access @Section . sub @Root) (inner @Left)
-navigation 'l' = outer @Right
-navigation 'h' = outer @Left
+navigation :: forall direction . (Morphed (Rotate direction) (Zipper List) (Maybe <:.> Zipper List)) => State (Zipper List Task) ()
+navigation = void . modify $ \z -> resolve @(Zipper List Task) identity z # run (rotate @direction z)
 
-inner :: forall direction . (Morphed (Rotate direction) (Zipper List) (Maybe <:.> Zipper List)) => State (Zipper List Task) ()
-inner = void . modify $ \z -> resolve @Cursor identity z # run (rotate @direction z)
-
-outer :: forall direction . (Morphed (Rotate direction) (Zipper List) (Maybe <:.> Zipper List)) => State (Zipper List Section) ()
-outer = void . modify $ \z -> resolve @(Zipper List Section) identity z # run (rotate @direction z)
-
-eventloop = (eventloop !.) =<< adapt . navigation =<< (adapt getChar !.) =<< refresh'
-
-load_tasks_zipper connection (title :*: q) =
-	((:*:) title <-|-) . run . into @(Zipper List) . list_to_list empty <-|- query_ @Task connection q
-
-queries :: Nonempty List := String :*: Query
-queries = into @(Nonempty List) . vectorize
-	$ (("Tasks for TODAY (1)" :: String) :*: today_tasks)
-	:*: (("Tasks for TODAY (2)" :: String) :*: today_tasks)
-	:*: (("Tasks for TODAY (3)" :: String) :*: today_tasks)
-	:*: (("Tasks for TODAY (4)" :: String) :*: today_tasks)
-
-sections :: Nonempty List (Maybe Section) -> Maybe :. Zipper List :. (:*:) String :. Zipper List := Task
-sections (Construct (Just x) (Just xs)) = Just $ twosome (Identity x) (twosome empty $ into @List $ TU @Covariant @Covariant xs)
-sections (Construct (Just x) Nothing) = Just $ twosome (Identity x) (twosome empty empty)
-sections (Construct Nothing (Just xs)) = run $ (into @(Zipper List) # into @List (TU @Covariant @Covariant xs))
-sections (Construct Nothing Nothing) = Nothing
+eventloop = (eventloop !.) =<< keystroke =<< (adapt getChar !.) =<< refresh
 
 main = do
 	connection <- open "facts.db"
 	today <- query_ @Task connection today_tasks
-	let Just section = run . into @(Zipper List) # list_to_list empty today
+	let Just tasks = run . into @(Zipper List) # list_to_list empty today
 	prepare_terminal
-	Just ss <- sections <-|- load_tasks_zipper connection <<- queries
-	--eventloop ! "Tasks for TODAY" :*: section
-	eventloop ! ss
+	eventloop ! tasks
--- Control/Pandora/SQLite.hs
+++ Control/Pandora/SQLite.hs
@@ -15,31 +15,31 @@ update_task_status = "UPDATE tasks SET status = ? WHERE id = ?"
 
 today_tasks :: Query
 today_tasks =
-	"SELECT tasks.id, status, task_event_priority, title, \
+	"SELECT tasks.id, status, mode, title, \
 	\strftime('%H:%M', start, 'unixepoch', 'localtime'), \
 	\IFNULL(strftime('%H:%M', stop, 'unixepoch', 'localtime'), '') \
 	\FROM tasks JOIN objectives on tasks.objective_id = objectives.id \
 	\WHERE start >= " <> start_of_today <> " AND IFNULL(stop <= " <> end_of_today <> ", 1) \
-	\ORDER BY status, task_event_priority, start;"
+	\ORDER BY status, mode, start;"
 
 tomorrow_tasks :: Query
 tomorrow_tasks =
-	"SELECT tasks.id, status, task_event_priority, title, \
+	"SELECT tasks.id, status, mode, title, \
 	\strftime('%H:%M', start, 'unixepoch', 'localtime'), \
 	\IFNULL(strftime('%H:%M', stop, 'unixepoch', 'localtime'), '') \
 	\FROM tasks JOIN objectives on tasks.objective_id = objectives.id \
 	\WHERE start >= " <> start_of_tomorrow <> " AND IFNULL(stop <= " <> end_of_tomorrow <> ", 1) \
-	\ORDER BY status, task_event_priority, start;"
+	\ORDER BY status, mode, start;"
 
 someday_todo :: Query
 someday_todo =
-	"SELECT tasks.id, 1, task_event_priority, title, strftime('%d.%m', start, 'unixepoch', 'localtime'), '' \
+	"SELECT tasks.id, 1, mode, title, strftime('%d.%m', start, 'unixepoch', 'localtime'), '' \
 	\FROM tasks JOIN objectives on tasks.objective_id = objectives.id \
-	\WHERE stop IS NULL AND status = 1 AND task_event_priority = 0;"
+	\WHERE stop IS NULL AND status = 1 AND mode = 0;"
 
 overdue :: Query
 overdue =
-	"SELECT tasks.id, -2, task_event_priority, title, strftime('%d.%m %H:%M', start, 'unixepoch', 'localtime'), strftime('%d.%m %H:%M', stop, 'unixepoch', 'localtime') \
+	"SELECT tasks.id, -2, mode, title, strftime('%d.%m %H:%M', start, 'unixepoch', 'localtime'), strftime('%d.%m %H:%M', stop, 'unixepoch', 'localtime') \
 	\FROM tasks JOIN objectives on tasks.objective_id = objectives.id \
 	\WHERE status = 1 and IFNULL(stop < strftime('%s', 'now'), 0) \
 	\ORDER BY start;"
