0c5ebc2272b1f7669cf0e26fed4cbe834876f9d0
Murat Kasimov <iokasimov.m@gmail.com>
2021-12-10 08:35:03 +0300

    Refactor: use zoom, overlook, access and perhaps for stateful actions

--- Control/Pandora/Main.hs
+++ Control/Pandora/Main.hs
@@ -39,8 +39,8 @@ show_task focused (_ :*: status :*: mode :*: oid :*: title :*: start :*: stop) =
 	show_task_boundaries 1 begin [] = "{" + begin + " - ....."
 	show_task_boundaries 1 begin complete = "{" + begin + " - " + complete + "} "
 
-display :: Maybe (ID Event) :*: Events :*: Tasks -> IO ()
-display (_ :*: events :*: tasks) = void $ do
+display :: Maybe (ID Event) :*: Events :*: Maybe Tasks -> IO ()
+display (_ :*: events :*: Just tasks) = void $ do
 	refresh_terminal
 	putStrLn $ "\n   \ESC[1m\ESC[4m" + "Events for today" + "\ESC[0m\n"
 	putStrLn . show_event <<- events
@@ -48,23 +48,6 @@ display (_ :*: events :*: tasks) = void $ do
 	putStrLn . show_task False -<<-<<- (Reverse <-|- view (sub @Left) tasks)
 	putStrLn . show_task True -<<-<<- view (sub @Root) tasks
 	putStrLn . show_task False -<<-<<- view (sub @Right) tasks
-
-confirm_change_status :: Status -> TUI (Maybe Status)
-confirm_change_status status = choice =<< keystroke
-	-*- (adapt . message =<< adapt title) -*- adapt refresh_terminal where
-
-	message :: String -> IO ()
-	message task = putStrLn
-		$ "\n   \ESC[4m" + "Are you sure you want to mark \""
-		+ task + "\" as [\ESC[7m" + show status + "\ESC[27m]? (Yes/No)\ESC[24m\n"
-
-	title :: State Model String
-	title = extract <-|- zoom @Model (access @String) current
-
-	choice :: Maybe ASCII -> TUI (Maybe Status)
-	choice (Just (Letter Upper Y)) = point # Just status
-	choice (Just (Letter Upper N)) = point # Nothing
-	choice _ = confirm_change_status status
 
 show_title False title = "\n   \ESC[4m" + title + "\ESC[0m"
 show_title True title = "\n + \ESC[1m\ESC[4m" + title + "\ESC[0m"
@@ -75,7 +58,7 @@ refresh = adapt . display =<< current
 type Clocked = Maybe (ID Event)
 type Events = List Event
 type Tasks = Tape List Task
-type Model = Clocked :*: Events :*: Tasks
+type Model = Clocked :*: Events :*: Maybe Tasks
 
 type TUI = Environment Connection :> State Model :> IO
 
@@ -84,35 +67,71 @@ instance Accessible field Task => Accessible field Tasks where
 
 handle :: Maybe ASCII -> TUI ()
 handle (Just (Letter Lower R)) = point ()
-handle (Just (Letter Lower J)) = adapt # zoom @Model @Tasks @(State _) access (navigation @Right)
-handle (Just (Letter Lower K)) = adapt # zoom @Model @Tasks @(State _) access (navigation @Left)
-handle (Just (Letter Upper D)) = void $ (change_status_in_db <<-) =<< confirm_change_status DONE
-handle (Just (Letter Upper T)) = void $ (change_status_in_db <<-) =<< confirm_change_status TODO
-handle (Just (Letter Upper G)) = void $ (change_status_in_db <<-) =<< confirm_change_status GONE
+handle (Just (Letter Lower J)) = adapt # navigate @Right
+handle (Just (Letter Lower K)) = adapt # navigate @Left
+handle (Just (Letter Upper D)) = void $
+	(change_status_in_db DONE <<-) . (identity =<<)
+	=<< (adapt . change_status_in_state DONE <<-) . (identity =<<)
+	=<< (adapt . confirm_change_status DONE <<-)
+	=<< adapt current_task_info
+handle (Just (Letter Upper T)) = void $
+	(change_status_in_db TODO <<-) . (identity =<<)
+	=<< (adapt . change_status_in_state TODO <<-) . (identity =<<)
+	=<< (adapt . confirm_change_status TODO <<-)
+	=<< adapt current_task_info
+handle (Just (Letter Upper G)) = void $
+	(change_status_in_db GONE <<-) . (identity =<<)
+	=<< (adapt . change_status_in_state GONE <<-) . (identity =<<)
+	=<< (adapt . confirm_change_status GONE <<-)
+	=<< adapt current_task_info
+
 handle (Just (Letter Upper I)) = point ()
 handle (Just (Letter Upper O)) = point ()
 handle c = point ()
 
-change_status_in_db :: Status -> TUI ()
-change_status_in_db new = identity =<< update_task_row <-|- env
-	<-*- zoom @Model (access @(ID ())) (unid . extract <-|- current)
-	<-*- zoom @Model (access @Status) (extract <-|- replace (Identity new))
+confirm_change_status :: Status -> ID () :*: String -> IO # Maybe (ID ())
+confirm_change_status status (id :*: title) = choice =<< keystroke -*- message title -*- refresh_terminal where
+
+	message :: String -> IO ()
+	message task = putStrLn
+		$ "\n   \ESC[4m" + "Are you sure you want to mark \""
+		+ task + "\" as [\ESC[7m" + show status + "\ESC[27m]? (Yes/No)\ESC[24m\n"
+
+	choice :: Maybe ASCII -> IO # Maybe (ID ())
+	choice (Just (Letter Upper Y)) = point # Just id
+	choice (Just (Letter Upper N)) = point # Nothing
+	choice _ = confirm_change_status status (id :*: title)
+
+current_task_info :: State Model # Maybe (ID () :*: String)
+current_task_info = zoom @Model (perhaps @Tasks) $ overlook $ (:*:)
+	<-|- zoom access (extract <-|- overlook (current @(ID ())))
+	<-*- zoom access (extract <-|- overlook (current @String))
+
+change_status_in_state :: Status -> ID () -> State Model # Maybe (ID ())
+change_status_in_state new id = zoom @Model (perhaps @Tasks) $
+	overlook . zoom access $ point id -*- overlook (replace @Status new)
+
+change_status_in_db :: Status -> ID () -> TUI ()
+change_status_in_db new id = identity =<< update_task_row <-|- env <-*- point (unid id) <-*- point new
 
 update_task_row :: Connection -> Int -> Status -> TUI ()
 update_task_row connection id status = adapt $ execute connection update_task_status (status, id)
 
-navigation :: forall direction . (Morphed (Rotate direction) (Tape List) (Maybe <:.> Tape List)) => State (Identity (Tape List Task)) ()
-navigation = void . modify $ \(Identity z) -> Identity $ resolve @(Tape List Task) identity z # run (rotate @direction z)
+navigate :: forall direction . (Morphed (Rotate direction) (Tape List) (Maybe <:.> Tape List)) => State Model ()
+navigate = void $ zoom @Model # access @(Maybe Tasks) # modify (move <-|-|-) where
 
-keystroke :: TUI (Maybe ASCII)
-keystroke = castASCII <-|- adapt getChar
+	move :: Tape List Task -> Tape List Task
+	move z = resolve @(Tape List Task) identity z # run (rotate @direction z)
+
+keystroke :: IO (Maybe ASCII)
+keystroke = castASCII <-|- getChar
 
 eventloop :: TUI ()
-eventloop = forever_ $ handle =<< keystroke -*- refresh
+eventloop = forever_ $ handle =<< adapt keystroke -*- refresh
 
 main = do
 	connection <- open "facts.db"
-	Just tasks <- run . into @(Tape List) . list_to_list empty <-|- query_ @Task connection today_tasks
+	tasks <- run . into @(Tape List) . list_to_list empty <-|- query_ @Task connection today_tasks
 	events <- list_to_list empty <-|- query_ @Event connection today_events
 	prepare_terminal
 	eventloop ! connection ! (Nothing :*: events :*: tasks)
